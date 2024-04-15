b18df51e3278e83470b1c43fe266ef4a1ae31247
mdpdlr <martindeperettidelarocca@gmail.com>
2024-04-15 15:54:14 +0200

    version 0

---
+++ src/Voiture.java
@@ -0,0 +1,31 @@
+public class Voiture {
+    private int voitureID;
+    private String modele;
+    private String marque;
+    private boolean type;
+    private float prix;
+    private boolean disponibilite;
+
+    public Voiture(int voitureID, String modele, String marque, boolean type, float prix, boolean disponibilite) {
+        this.voitureID = voitureID;
+        this.modele = modele;
+        this.marque = marque;
+        this.type = type;
+        this.prix = prix;
+        this.disponibilite = disponibilite;
+    }
+
+    // Getters et setters
+
+    public boolean isDisponible() {
+        return this.disponibilite;
+    }
+
+    public void louer() {
+        this.disponibilite = false;
+    }
+
+    public void retour() {
+        this.disponibilite = true;
+    }
+}
