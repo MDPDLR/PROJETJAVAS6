da8cf7918a84ae301fe0f2e82ef94f9add113103
paulo2c <96883322+paulo2c@users.noreply.github.com>
2024-04-16 16:35:53 +0200

    Update ClientDAO.java

--- src/DAO/ClientDAO.java
+++ src/DAO/ClientDAO.java
@@ -1,4 +1,8 @@
 package DAO;
+import java.util.List;
 
-public class ClientDAO {
+public interface ClientDAO {
+    Client getClientByEmail(String email);
+    void insertClient(Client client);
+    void updateClient(Client client);
 }
