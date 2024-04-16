e0dde1016739b3769c8e724d3798eeabfddcc2e1
paulo2c <96883322+paulo2c@users.noreply.github.com>
2024-04-16 16:59:03 +0200

    Update AdminDAOImpl.java

--- src/DAO/AdminDAOImpl.java
+++ src/DAO/AdminDAOImpl.java
@@ -1,4 +1,69 @@
 package DAO;
 
-public class AdminDAOImpl {
+public class AdminDAOImpl implements AdminDAO {
+    private Connection connection;
+
+    // Constructeur prenant une connexion à la base de données
+    public AdminDAOImpl(Connection connection) {
+        this.connection = connection;
+    }
+
+   @Override
+public void insertAdmin(Admin admin) {
+    String query = "INSERT INTO Admin (Nom, IDemploye, mdpEmploye) VALUES (?, ?, ?)";
+    
+    try (PreparedStatement statement = connection.prepareStatement(query)) {
+        statement.setString(1, admin.getNom());
+        statement.setInt(2, admin.getIdEmploye());
+        statement.setString(3, admin.getMdpEmploye());
+        
+        int rowsInserted = statement.executeUpdate();
+        if (rowsInserted > 0) {
+            System.out.println("Nouvel administrateur inséré avec succès.");
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+}
+
+    @Override
+public void updateAdmin(Admin admin) {
+    String query = "UPDATE Admin SET Nom = ?, mdpEmploye = ? WHERE IDemploye = ?";
+    
+    try (PreparedStatement statement = connection.prepareStatement(query)) {
+        statement.setString(1, admin.getNom());
+        statement.setString(2, admin.getMdpEmploye());
+        statement.setInt(3, admin.getIdEmploye());
+        
+        int rowsUpdated = statement.executeUpdate();
+        if (rowsUpdated > 0) {
+            System.out.println("Informations de l'administrateur mises à jour avec succès.");
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+}
+
+   
+@Override
+public Admin getAdminById(int idEmploye) {
+    String query = "SELECT * FROM Admin WHERE IDemploye = ?";
+    
+    try (PreparedStatement statement = connection.prepareStatement(query)) {
+        statement.setInt(1, idEmploye);
+        
+        ResultSet resultSet = statement.executeQuery();
+        if (resultSet.next()) {
+            String nom = resultSet.getString("Nom");
+            String mdpEmploye = resultSet.getString("mdpEmploye");
+            
+            return new Admin(idEmploye, nom, mdpEmploye);
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+    
+    return null;
+}
+
 }
